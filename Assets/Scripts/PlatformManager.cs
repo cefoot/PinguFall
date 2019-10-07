86dd2c7d73111e745fbc5b218354eb492ad38b3c
Papenfuß Chris <chris.papenfuss@data-experts.de>
2019-10-07 13:52:39 +0200

    fine tuning weights

--- Assets/Scripts/PlatformManager.cs
+++ Assets/Scripts/PlatformManager.cs
@@ -10,13 +10,17 @@ public class PlatformManager : MonoBehaviour
     private Dictionary<Transform, Vector3> _adjacents = new Dictionary<Transform, Vector3>();
     private System.Random _myRand = new System.Random();
     private Transform[] _orderedAdjacents;
+    private Rigidbody _myRigid;
 
+    public float MinProbabilityRange = 0.55F;
+    public float MaxProbabilityRange = 1F;
     public int AdjacentsCount = 6;
     [Range(0f, 1f)]
-    public float FallProbability = 0.9F;
+    public float FallProbability = 0.08F;
 
     private void OnEnable()
     {
+        _myRigid = GetComponent<Rigidbody>();
         var myCollider = GetComponentInChildren<Collider>();
         var hits = Physics.SphereCastAll(new Ray(myCollider.bounds.center + (Vector3.down * myCollider.bounds.extents.y * 2F), Vector3.up), myCollider.bounds.extents.magnitude * 1.1F, 2F);
         foreach (var item in hits)
@@ -33,17 +37,12 @@ public class PlatformManager : MonoBehaviour
         AdjacentsCount = _adjacents.Count;
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
+        if (_myRigid.useGravity) Drop();
         var dropped = (from adj in _adjacents
-                       where Vector3.Distance(adj.Key.position, adj.Value) > MIN_DROP_DISTANCE
+                       where adj.Key && Vector3.Distance(adj.Key.position, adj.Value) > MIN_DROP_DISTANCE
                        select adj.Key).ToList();
         var newDropped = false;
         foreach (var item in dropped)
@@ -58,37 +57,49 @@ public class PlatformManager : MonoBehaviour
         }
     }
 
+    public void Drop()
+    {
+        _myRigid.useGravity = true;
+        enabled = false;
+        _myRigid.isKinematic = false;
+    }
+
+
     private void ShouldFall()
     {
-        var tries = 5;
-        for (int i = 0; i < 3; i++)
+        var tries = 10;
+        for (var i = 0; i < 3; i++)
         {
             //two on opposite 
             if (_orderedAdjacents.Length > i + 3 && _adjacents.ContainsKey(_orderedAdjacents[i]) && _adjacents.ContainsKey(_orderedAdjacents[i + 3]))
             {
-                tries--;
+                tries -= 2;
             }
         }
-        for (int i = 0; i < 2; i++)
+        for (var i = 0; i < 2; i++)
         {
             //three in triangle 
             if (_orderedAdjacents.Length > i + 4 && _adjacents.ContainsKey(_orderedAdjacents[i]) && _adjacents.ContainsKey(_orderedAdjacents[i + 2]) && _adjacents.ContainsKey(_orderedAdjacents[i + 4]))
             {
-                tries--;
+                tries -= 2;
             }
         }
-
-        for (int i = 0; i < tries; i++)
+        var probs = new List<float>();
+        var prob = 1F;
+        for (var i = 0; i < tries; i++)
         {
-            float v = UnityEngine.Random.Range(0F, 1F);
-            if (v > FallProbability)
-            {
-
-                Debug.Log($"{name}: falling (Tries:{tries} Prob:{v})");
-                GetComponent<Rigidbody>().useGravity = true;
-                return;
-            }
+            var v = UnityEngine.Random.Range(MinProbabilityRange, MaxProbabilityRange);
+            probs.Add(v);
+            prob *= v;
         }
-        Debug.Log($"{name}: not falling");
+        if (prob < FallProbability)
+        {
+            Debug.Log($"{name}: falling ([{probs.Count}]{String.Concat(probs.ToArray())}:\r\n{prob})");
+            Drop();
+        }
+        else
+        {
+            Debug.Log($"{name}: NOT falling ([{probs.Count}]{String.Concat(probs.ToArray())}:\r\n{prob})");
+        }
     }
 }
