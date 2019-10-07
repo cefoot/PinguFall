c084b7c4ac01739e65f74e287953e724281d9ba8
cefoot <cefoot@outlook.de>
2019-10-07 20:15:56 +0200

    adding mouseinput making the game playable

---
+++ Assets/Scripts/MouseInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseInput : MonoBehaviour
+{
+
+    public GameObject Particle;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out var hit))
+            {
+                hit.collider.SendMessageUpwards("Drop", SendMessageOptions.DontRequireReceiver);
+                if (Particle) Instantiate(Particle, hit.point, Quaternion.identity);
+
+            }
+        }
+    }
+}
